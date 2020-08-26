97e439e59b7909f0bc47db562fc66c41ce39673a
ekastan <kawa130484@gmail.com>
2020-08-27 03:43:41 +0700

    tambah_layanan

---
+++ app/src/main/java/com/example/pratamajambipelayangan/LayananOnline.java
@@ -0,0 +1,14 @@
+package com.example.pratamajambipelayangan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayananOnline extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layanan_online);
+    }
+}
